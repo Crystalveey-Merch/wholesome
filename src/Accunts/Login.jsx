0ddd94ef0757baf8fd1728aa6871c42bc8fbb604
Udofa Daniel <danieludofa452@gmail.com>
2023-10-31 10:29:13 +0100

    ....

--- src/Accunts/Login.jsx
+++ src/Accunts/Login.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React from "react";
 import {
-    signInWithEmailAndPassword,
-    GoogleAuthProvider,
-    signInWithPopup,
+  signInWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
 } from "firebase/auth";
 import { useState } from "react";
 // import {au } from "../../node_modules/firebase/auth";
@@ -16,116 +16,118 @@ import { db } from "../firebase/auth";
 import { doc, getDoc, setDoc, updateDoc } from "firebase/firestore";
 
 const Login = () => {
+  const navigate = useNavigate();
+  const [error, setError] = useState(null);
+  const [email, setEmail] = useState("");
 
-    const navigate = useNavigate();
-    const [error, setError] = useState(null);
-    const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const [password, setPassword] = useState("");
+  const passwordChangeHandler = (event) => {
+    setPassword(event.target.value);
+  };
+  const [showPassword, setShowPassword] = useState(false);
 
-    const passwordChangeHandler = (event) => {
-        setPassword(event.target.value);
-    };
-    const [showPassword, setShowPassword] = useState(false);
-  
-    const handleTogglePassword = (e) => {
-        e.preventDefault();
-        setShowPassword(!showPassword);
-    };
-  
-    const loginhandler = (e) => {
-        e.preventDefault();
-        signInWithEmailAndPassword(auth, email, password)
+  const handleTogglePassword = (e) => {
+    e.preventDefault();
+    setShowPassword(!showPassword);
+  };
 
+  const loginhandler = (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        toast.success(
+          <div className="text-black text-sm ">Login Successful</div>
+        );
 
-            .then((userCredential) => {
-                
-                toast.success(<div className="text-black text-sm ">Login Successful</div>);
+        navigate("/dashboard/profile");
+      })
+      .catch((err) => {
+        setError(err.message);
+      });
+  };
+  return (
+    <div>
+      <div className="  pt-40 bg-stone-100 text-black w-screen h-screen ">
+        <div className="text-center text-2xl">
+          <div className="pt-34  m-auto   ">
+            <div className=" h-full w-full flex rounded rounded-lg    ">
+              <div className="w-2/5 m-auto  mt-36 sm:mt-20 sm:w-full sm:h-full mb-44 rounded">
+                <div className="bg-white  flex flex-col text-xl rounded-2xl">
+                  <div className="container  flex flex-col items-center justify-center px-10 sm:px-2 shadow">
+                    <div className="  p-10  text-black w-full">
+                      <h1 className="mb-8 text-xl text-center text-red-500">
+                        Login
+                      </h1>
 
-                navigate("/dashboard/profile");
+                      <form onSubmit={loginhandler} className="form-control">
+                        <input
+                          value={email}
+                          onChange={(e) => setEmail(e.target.value)}
+                          type="email"
+                          required
+                          className="block border border-grey-light bg-gray-100 w-full p-3 rounded mb-4"
+                          name="email"
+                          placeholder="Email"
+                        />
 
-            })
-            .catch((err) => {
-                setError(err.message);
-            });
-        }
-        return (
-            <div>
+                        <span className="flex mt-3 gap-2 mb-4 border-grey-light bg-gray-100 w-full  rounded">
+                          <input
+                            value={password}
+                            type={showPassword ? "text" : "password"}
+                            id="password"
+                            required
+                            className="block  bg-gray-100 w-full p-3"
+                            name="password"
+                            placeholder="Password"
+                            onChange={passwordChangeHandler}
+                          />
+                          <div
+                            onClick={handleTogglePassword}
+                            className="bg-transparent centre m-auto p-2 border-none"
+                          >
+                            {showPassword ? (
+                              <FontAwesomeIcon
+                                icon={faEye}
+                                className="text-red-500 text-sm"
+                              />
+                            ) : (
+                              <FontAwesomeIcon
+                                icon={faEyeSlash}
+                                className="text-red-500 text-sm"
+                              />
+                            )}
+                          </div>
+                        </span>
 
-                <div className='  pt-10 bg-white text-black '
-                >
-
-                    <div className='text-center text-2xl'>
-                        <div className="pt-34  m-auto   ">
-                            <div className=" h-full w-full flex rounded rounded-lg    ">
-                                <div className='w-1/2 text-lg h-screen  sm:hidden ' style={{ backgroundImage: "url(/Images/world.jpeg)", backgroundPosition: "center", backgroundRepeat: "none", backdropFilter: "black" }}>
-
-                                </div>
-                                <div className='w-1/2 sm:m-auto pt-36 sm:pt-20 sm:w-full sm:h-full pb-44 '>
-                                    <div className="bg-grey-lighter  flex flex-col text-xl">
-                                        <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
-                                            <div className=" px-6 py-8  text-black w-full">
-                                                <h1 className="mb-8 text-xl text-center text-red-500">Login</h1>
-
-                                           <form onSubmit={loginhandler} className="form-control">   
-                                                <input
-                                                    value={email}
-                                                    onChange={(e) => setEmail(e.target.value)}
-                                                    type="email"
-                                                    required
-                                                    className="block border border-grey-light bg-gray-100 w-full p-3 rounded mb-4"
-                                                    name="email"
-                                                    placeholder="Email"
-                                                />
-
-                                                <span className="flex mt-3 gap-2 mb-4 border-grey-light bg-gray-100 w-full  rounded">
-                                                    <input
-                                                        value={password}
-                                                        type={showPassword ? "text" : "password"}
-                                                        id="password"
-                                                        required
-                                                        className="block  bg-gray-100 w-full p-3"
-                                                        name="password"
-                                                        placeholder="Password"
-                                                        onChange={passwordChangeHandler}
-
-                                                    />
-                                                    <div
-                                                        onClick={handleTogglePassword}
-                                                        className="bg-transparent centre m-auto p-2 border-none"
-                                                    >
-                                                        {showPassword ? (
-                                                            <FontAwesomeIcon icon={faEye} className="text-red-500 text-sm" />) : (
-
-                                                            <FontAwesomeIcon icon={faEyeSlash} className="text-red-500 text-sm" />)}
-                                                    </div>
-                                                </span>
-
-                                                <button
-                                                    type="submit"
-                                                    className="w-full text-center py-3 rounded bg-green-500 text-white hover:bg-green-dark focus:outline-none my-1"
-                                                >Login</button>
-                                                </form>  
-
-                                            </div>
-
-                                            <div className="text-grey-dark mt-6">
-                                                Dont have an account?
-                                                <NavLink className="no-underline border-b border-blue text-blue" to="/signup">
-                                                    Sign up
-                                                </NavLink>.
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div>
+                        <button
+                          type="submit"
+                          className="w-full text-center py-3 rounded bg-green-500 text-white hover:bg-green-dark focus:outline-none my-1"
+                        >
+                          Login
+                        </button>
+                      </form>
                     </div>
 
+                    <div className="text-grey-dark mt-6">
+                      Dont have an account?
+                      <NavLink
+                        className="no-underline border-b border-blue text-blue"
+                        to="/signup"
+                      >
+                        Sign up
+                      </NavLink>
+                      .
+                    </div>
+                  </div>
                 </div>
+              </div>
             </div>
-        )
-    }
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-    export default Login
+export default Login;
--- src/Accunts/Signip.tsx
+++ src/Accunts/Signip.tsx
@@ -99,22 +99,12 @@ const Signip = () => {
 
   return (
     <div>
-      <div className="  pt-10 bg-white text-black w-screen ">
+      <div className="  my-20 bg-white text-black w-screen ">
         <div className="text-center text-2xl">
-          <div className="pt-34  m-auto   ">
+          <div className="  m-auto   ">
             <div className=" h-full w-full flex rounded rounded-lg    ">
-              <div
-                className="w-1/2 text-lg h-screen  sm:hidden "
-                style={{
-                  backgroundImage: "url(/Images/world.jpeg)",
-                  backgroundPosition: "center",
-                  backgroundRepeat: "none",
-                  backdropFilter: "black",
-                }}
-              >
-              
-              </div>
-              <div className="w-1/2 sm:m-auto pt-36 sm:pt-10 sm:w-full sm:h-full ">
+             
+              <div className="w-2/5 m-auto  sm:w-full sm:h-full ">
                 <div className="bg-grey-lighter  flex flex-col text-xl">
                   <div className="container max-w-xl mx-auto flex-1 flex flex-col items-center justify-center px-2">
                     <div className=" px-6 py-8  text-black w-full">
