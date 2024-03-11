0e918e71117e9971f419a2e5cbd911d8b51fd581
muzardemoses <muzardemoses@gmail.com>
2024-03-11 08:57:06 +0100

    ...

--- src/Interest/Discover.jsx
+++ src/Interest/Discover.jsx
@@ -10,30 +10,31 @@ export const Discover = ({ interests }) => {
         <h2 className="text-2xl font-bold text-black sm:text-xl">
           Discover Interest Groups
         </h2>
-        <div className="grid grid-cols-4 gap-6 pb-10 lg:grid-cols-3 md:grid-cols-2 md:w-full">
+        <div className="flex flex-wrap gap-10 gap-y-6">
           {interests.map((interest) => (
-            <div
+            <Link
+              to={`/i/${convertToLowercase(interest.name)}`}
               key={interest.id}
-              className="w-60 border border-gray-200 rounded-md flex flex-col gap2 justify-start items-start lg:w-56 md:w-full"
+              className="w-max min-w-[150px] p-2 border border-gray-200 rounded-full flex gap-2 justify-start items-center transition ease-in-out duration-300 hover:bg-gray-100 cursor-pointer"
             >
               <img
                 src={interest.wallPaper}
                 alt={interest.name}
-                className="w-full h-24 object-cover rounded-t-md"
+                className="w-10 max-w-10 h-10 max-w-[40px] rounded-full object-cover"
               />
-              <div className="flex flex-col gap-2 p-3 w-full">
+              <div className="flex flex-col gap-2 w-full">
                 <h3 className="text-base font-semibold text-black sm:text-[0.95rem] one-line-text">
                   {interest.name}
                 </h3>
                 {/* <p className="text-gray-500">{interest.description}</p> */}
-                <Link
+                {/* <Link
                   to={`/i/${convertToLowercase(interest.name)}`}
                   className="w-full px-4 py-2 border border-[#ff5841] rounded-md text-[#ff5841] font-semibold text-center"
                 >
                   Join
-                </Link>
+                </Link> */}
               </div>
-            </div>
+            </Link>
           ))}
         </div>
       </div>
